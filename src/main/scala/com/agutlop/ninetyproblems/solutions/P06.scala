e2cd2f3bf0d6e27fced3b7b9c3b59cc781cceaf9
a.gutierrez.lopez.contractor <a.gutierrez.lopez.contractor@bbva.com>
2018-03-26 15:08:38 +0200

    P06

---
+++ src/main/scala/com/agutlop/ninetyproblems/solutions/P06.scala
@@ -0,0 +1,11 @@
+package com.agutlop.ninetyproblems.solutions
+
+/**
+  * P06 (*) Find out whether a list is a palindrome.
+  * Example:
+  * scala> isPalindrome(List(1, 2, 3, 2, 1))
+  * res0: Boolean = true
+  */
+class P06 {
+
+}
