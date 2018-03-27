829f69666278d4f25276f8d2479cceab4800b3ce
a.gutierrez.lopez.contractor <a.gutierrez.lopez.contractor@bbva.com>
2018-03-27 11:53:23 +0200

    P14 solution

---
+++ src/main/scala/com/agutlop/ninetyproblems/solutions/lists/P14.scala
@@ -0,0 +1,13 @@
+package com.agutlop.ninetyproblems.solutions.lists
+
+/**
+  * P14 (*) Duplicate the elements of a list.
+  * Example:
+  * scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+  * res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+  */
+object P14 {
+
+  def duplicate[A](ls: List[A]): List[A] = ls.flatMap(e => List(e, e))
+
+}
---
+++ src/test/scala/com/agutlop/ninetyproblems/solutions/lists/P14Test.scala
@@ -0,0 +1,13 @@
+package com.agutlop.ninetyproblems.solutions.lists
+
+import org.scalatest.FunSuite
+import com.agutlop.ninetyproblems.solutions.lists.P14._
+
+class P14Test extends FunSuite{
+
+  test("encode a list and an empty list") {
+    assert(duplicate(List('a, 'b, 'c, 'c, 'd)) == List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+    assert(duplicate(List()) == List())
+  }
+
+}
